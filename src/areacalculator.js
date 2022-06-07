c5568f9d1a193b03e7216bd7956a96ca38a17f19
kevin.guerrero <kevin.guerrero@newshore.es>
2022-06-06 21:48:48 -0500

    implement area calculator

--- src/areacalculator.js
+++ src/areacalculator.js
@@ -1,9 +1,36 @@
 const pi = 3.14;
 
 const calculateArea = (choice,side,length,breadth,radius) =>{   
+    if (!choice) {
+        return -1;
+    }
     let area = 0.0; 
     // write logic here
     // note that you check the values passed are not null before performing any operation on them
+
+    switch(choice) {
+        case 'rectangle': 
+            if (breadth <= 0 || length <= 0 || !breadth || !length) {
+                return -1;
+            }
+            area = breadth * length;
+        break;
+        case 'square':
+            if (side <= 0 || !side) {
+                return -1;
+            }
+            area = side*4;
+        break;
+        case 'circle':
+            if (radius <= 0 || !radius) {
+                return -1;
+            }
+            area = 3.14 * radius * radius;
+        break;
+        default :
+            return -1;
+    }
+
     return area
 }
 module.exports = {calculateArea}
