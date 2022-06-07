98e5c2a02f8a5b5a7f8fe6f3e478a669321f1a3c
kevin.guerrero <kevin.guerrero@newshore.es>
2022-06-06 21:20:58 -0500

    add test to the calculator

---
+++ src/areacalculator.js
@@ -0,0 +1,9 @@
+const pi = 3.14;
+
+const calculateArea = (choice,side,length,breadth,radius) =>{   
+    let area = 0.0; 
+    // write logic here
+    // note that you check the values passed are not null before performing any operation on them
+    return area
+}
+module.exports = {calculateArea}
